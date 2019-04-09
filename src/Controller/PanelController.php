ce70e1db5b552899e13f9813020300791732cd37
mikigal <mikigal.priv@gmail.com>
2019-04-10 01:39:17 +0200

    Added user settings view

--- src/Controller/PanelController.php
+++ src/Controller/PanelController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\PasteRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
 
 /**
  * @Route("/panel")
@@ -18,9 +20,10 @@ class PanelController extends AbstractController {
      * @IsGranted("ROLE_USER")
      * @Route("/", name="app_panel")
      */
-    public function root(PasteRepository $pasteRepository) {
+    public function root(Request $request, PasteRepository $pasteRepository) {
         return $this->render("panel/panel.html.twig", [
-            'sidebar' => $pasteRepository->getSidebar()
+            'sidebar' => $pasteRepository->getSidebar(),
+            'password' => $request->get('password')
         ]);
     }
 
@@ -30,6 +33,9 @@ class PanelController extends AbstractController {
      */
     public function pastes(Request $request, PasteRepository $pasteRepository) {
         $user = $this->get('security.token_storage')->getToken()->getUser();
+        if ($user == 'anon.') {
+            return $this->redirectToRoute('app_root');
+        }
 
         return $this->render('panel/pastes.html.twig', [
             'sidebar' => $pasteRepository->getSidebar(),
@@ -45,6 +51,9 @@ class PanelController extends AbstractController {
     public function delete(string $name, PasteRepository $pasteRepository, EntityManagerInterface $entityManager) {
         $paste = $pasteRepository->findOneByName($name);
         $user = $this->get('security.token_storage')->getToken()->getUser();
+        if ($user == 'anon.') {
+            return $this->redirectToRoute('app_root');
+        }
 
         if ($paste == null) {
             return  $this->redirectToRoute('app_pastes', [
@@ -63,5 +72,41 @@ class PanelController extends AbstractController {
         return  $this->redirectToRoute('app_pastes', [
             'removed' => true
         ]);
+    }
+
+    /**
+     * @IsGranted("ROLE_USER")
+     * @Route("/password/{id}", name="app_password")
+     */
+    public function password(Request $request, int $id, EncoderFactoryInterface $encoderFactory, EntityManagerInterface $entityManager) {
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        if ($user == 'anon.' || $user->getId() != $id) {
+            return $this->redirectToRoute('app_panel');
+        }
+
+        $old = $request->get('old');
+        $new = $request->get('new');
+        $repeat = $request->get('repeat');
+
+        if ($old == null || $new == null || $repeat == null) {
+            return $this->redirectToRoute('app_panel');
+        }
+
+        if ($new != $repeat) {
+            return $this->redirectToRoute('app_panel', ['password' => 2]);
+        }
+
+        if (!$encoderFactory->getEncoder(User::class)->isPasswordValid($user->getPassword(), $old, $user->getSalt())) {
+            return $this->redirectToRoute('app_panel', ['password' => 3]);
+        }
+
+        if ($old != $new) {
+            return $this->redirectToRoute('app_panel', ['password' => 4]);
+        }
+        $user->setPassword($encoderFactory->getEncoder(User::class)->encodePassword($new, $user->getSalt()));
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_panel', ['password' => 1]);
     }
 }
